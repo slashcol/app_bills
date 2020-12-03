36f1599b6d1c507aadd7c1e166620d30ba435fb8
unknown <g1770@hotmail.com>
2020-12-02 22:05:23 -0500

    First commit before deployment

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  serverUrl: 'https://ionshop.bills.pet/api',
+  appUrl: 'https://www.bills.pet',
+  appImageUrl: 'https://www.bills.pet/assets/imgs/ionshop.png',
+  appId: 'hcDzr5W7Qt',
+  fbId: '789454091616641',
+  stripePublicKey: 'pk_test_51HFhRDGzSWOHkwi6XGvnmqsEpLwBIKDkvKv4jyrHmn15Iv0toDWelpolk1i5wVLPjz9lA3P4cotfRYxENfZmZLnr00aMVGHo0T',
+  androidHeaderColor: '#222428',
+  defaultLang: 'es',
+  googleClientId: '597871700054-2e5dbhigvaehei7s5ktlprk0fuuat05k.apps.googleusercontent.com',
+  currency: {
+    code: 'COP',
+    display: 'symbol',
+    digitsInfo: '1.0-2',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,30 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  serverUrl: 'https://ionshop.bills.pet/api',
+  appUrl: 'https://www.bills.pet',
+  appImageUrl: 'https://www.bills.pet/assets/imgs/ionshop.png',
+  appId: 'hcDzr5W7Qt',
+  fbId: '789454091616641',
+  stripePublicKey: 'pk_test_51HFhRDGzSWOHkwi6XGvnmqsEpLwBIKDkvKv4jyrHmn15Iv0toDWelpolk1i5wVLPjz9lA3P4cotfRYxENfZmZLnr00aMVGHo0T',
+  androidHeaderColor: '#222428',
+  defaultLang: 'es',
+  googleClientId: '597871700054-2e5dbhigvaehei7s5ktlprk0fuuat05k.apps.googleusercontent.com',
+  currency: {
+    code: 'COP',
+    display: 'symbol',
+    digitsInfo: '1.0-2',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
